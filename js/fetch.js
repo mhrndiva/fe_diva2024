4ae95683b08944994e2075fc21abeaef51a243dd
mhrndiva <ariadiva00@gmail.com>
2024-04-26 15:35:58 +0700

    klmn

--- js/fetch.js
+++ js/fetch.js
@@ -1,11 +1,21 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
-import { setInner } from "https://bukulapak.github.io/element/process.js";
-let urlAPI = "https://ws-maharani2024-db57da935d66.herokuapp.com/presensi";
-get(urlAPI,isiTablePresensi);
-function isiTablePresensi(results){
-    console.log(results);
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import { isiTabel } from "./table.js";
+let urlAPI = "https://ws-indra2024-878f7e6fab92.herokuapp.com/presensi";
+get(urlAPI, isiTablePresensi);
+function isiTablePresensi(results) {
     results.forEach(isiRow);
 }
-function isiRow(value){
-    console.log(value)
+function isiRow(value) {
+    let content = 
+    isiTabel.replace("#NAMA#", value.biodata.nama)
+            .replace("#NOHP#", value.biodata.phone_number)
+            .replace("#JABATAN#", value.biodata.jabatan)
+            .replace("#LOKASI#", value.location)
+            .replace("#STATUS#", value.checkin)
+            .replace("#HARIKERJA#", value.biodata.hari_kerja)
+            .replace("#WARNA#", getRandomColor())
+            .replace(/#WARNALOGO#/g, getRandomColorName());
+        addInner("iniTabel", content);
 }
